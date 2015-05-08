42b295f913e5fe18f7eda3618be6139929e1be04
Sonia Trehan <soniatrehan6@gmail.com>
2015-05-08 15:08:31 -0400

    WOOHOOOOOO

--- panorama.js
+++ panorama.js
@@ -7,36 +7,15 @@ var servolib = require('servo-pca9685');
 var servo = servolib.use(tessel.port['C']);
 
 var servo1 = 1; // We have a servo plugged in at position 1
+var position = 0;
 
 servo.on('ready', function () {
-    var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
-    camera.on('ready', function() {
-        //notificationLED.high();
-        // Take the picture
+      //  Target position of the servo between 0 (min) and 1 (max).
 
-        var interval = setInterval(snapShot,500);
-
+    servo.configure(servo1, 0.05, 0.12, function () {
 
     });
-    //  Set the minimum and maximum duty cycle for servo 1.
-    //  If the servo doesn't move to its full extent or stalls out
-    //  and gets hot, try tuning these values (0.05 and 0.12).
-    //  Moving them towards each other = less movement range
-    //  Moving them apart = more range, more likely to stall and burn out
-    //servo.configure(servo1, 0.05, 0.4, function () {
-    //    setInterval(function () {
-    //        console.log('Position (in range 0-1):', position);
-    //        //  Set servo #1 to position pos.
-    //        servo.move(servo1, position);
-    //
-    //        // Increment by 10% (~18 deg for a normal servo)
-    //        position += 0.2;
-    //        if (position > 1) {
-    //            position = 0; // Reset servo position
-    //        }
-    //    }, 500); // Every 500 milliseconds
 });
-
 
 //    //server.listen(1337, function () {
 //    //    console.log('Server listening!');
@@ -44,7 +23,9 @@ servo.on('ready', function () {
 //
 var count = 0;
 
-
+camera.on('ready', function(){
+    var interval = setInterval(snapShot, 500);
+})
 
 
 
@@ -59,17 +40,17 @@ var snapShot = function(){
     }
     else {
         count++;
+
+
         camera.takePicture(function(err, image) {
             if (err) {
                 console.log('error taking image', err);
             } else {
-                servo.configure(servo1, 0.05, 0.4, function () {
-                    servo.move(servo1, position);
-                    position += 0.1;
-                            if (position > 1) {
-                                position = 0; // Reset servo position
-                            }
-                });
+                servo.move(servo1, position);
+                position += 0.2;
+                if (position > 1) {
+                    position = 0; // Reset servo position
+                }
                 console.log(count);
                 //notificationLED.low();
                 // Name the image
@@ -83,4 +64,3 @@ var snapShot = function(){
         });
     }
 }
-
