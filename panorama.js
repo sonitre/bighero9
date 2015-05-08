4d44f9cd84a7102d3ab83414d68c6012803c3661
Sonia Trehan <soniatrehan6@gmail.com>
2015-05-08 13:05:16 -0400

    still malfunctioning..

--- panorama.js
+++ panorama.js
@@ -7,38 +7,86 @@ var notificationLED = tessel.led[3]; // Set up an LED to notify when we're takin
 var count = 0;
 
 // Wait for the camera module to say it's ready
+//camera.on('ready', function() {
+//    notificationLED.high();
+//
+//    //server.listen(1337, function () {
+//    //    console.log('Server listening!');
+//    //});
+//
+//    // Take the picture
+//    while(count < 10){
+//        setInterval(function(){
+//        console.log(count);
+//            camera.takePicture(function(err, image) {
+//                if (err) {
+//                    console.log('error taking image', err);
+//                } else {
+//
+//                    notificationLED.low();
+//                    // Name the image
+//                    var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
+//                    // Save the image
+//                    console.log('Picture saving as', name, '...');
+//                    process.sendfile(name, image);
+//                    console.log('done.');
+//                    count++;
+//                }
+//            });
+//
+//        }, 1000);
+//    }
+//
+//
+//});
+var count = 0;
+
+
 camera.on('ready', function() {
-    notificationLED.high();
+    if(count < 3){
+        notificationLED.high();
+        // Take the picture
 
-    //server.listen(1337, function () {
-    //    console.log('Server listening!');
-    //});
-
-    // Take the picture
-    while(count < 10){
-        setInterval(function(){
-        console.log(count);
-            camera.takePicture(function(err, image) {
-                if (err) {
-                    console.log('error taking image', err);
-                } else {
-
-                    notificationLED.low();
-                    // Name the image
-                    var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
-                    // Save the image
-                    console.log('Picture saving as', name, '...');
-                    process.sendfile(name, image);
-                    console.log('done.');
-                    count++;
-                }
-            });
-
-        }, 1000);
+        interval = setInterval(snapShot,500);
+        var stopInterval = function(){
+            console.log('made it');
+            clearInterval(interval);
+        }
     }
 
-
 });
+
+
+camera.on('error', function(err) {
+    console.error(err);
+});
+
+var snapShot = function(){
+    if(count >= 3){
+        console.log('in hereee')
+        stopInterval();
+
+       camera.disable();
+        camera.takePicture = function(){'no photo for you!'};
+    }
+    else {
+        camera.takePicture(function(err, image) {
+            if (err) {
+                console.log('error taking image', err);
+            } else {
+                console.log(count);
+                notificationLED.low();
+                // Name the image
+                var name = 'picture-' + Date.now() + '.jpg';
+                // Save the image
+                console.log('Picture saving as', name, '...');
+                process.sendfile(name, image);
+                console.log('done.');
+                count++;
+            }
+        });
+    }
+}
 
 camera.on('error', function(err) {
     console.error(err);
