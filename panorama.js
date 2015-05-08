35328856741fbd9ed352d212c0963c7523409501
Sonia Trehan <soniatrehan6@gmail.com>
2015-05-08 15:48:03 -0400

    broken again

--- panorama.js
+++ panorama.js
@@ -1,31 +1,36 @@
 var http = require('http');
-var server = http.createServer();
 var tessel = require('tessel');
 var camera = require('camera-vc0706').use(tessel.port['A']);
 var servolib = require('servo-pca9685');
+
+var server = http.createServer(function(req, res){
+    res.writeHead( 200, {'Content-Type': 'text/plain'} );
+    res.end('Hello World!\n');
+}).listen(1337);
+
 
 var servo = servolib.use(tessel.port['C']);
 
 var servo1 = 1; // We have a servo plugged in at position 1
 var position = 0;
 
-servo.on('ready', function () {
-      //  Target position of the servo between 0 (min) and 1 (max).
 
+
+
+servo.on('ready', function () {
     servo.configure(servo1, 0.05, 0.12, function () {
 
     });
 });
 
-//    //server.listen(1337, function () {
-//    //    console.log('Server listening!');
-//    //});
-//
-var count = 0;
-
 camera.on('ready', function(){
     var interval = setInterval(snapShot, 500);
-})
+});
+
+
+var count = 0;
+
+
 
 
 
@@ -36,7 +41,7 @@ camera.on('error', function(err) {
 var snapShot = function(){
     if(count >= 7){
         clearInterval(interval);
-       camera.disable();
+        camera.disable();
     }
     else {
         count++;
@@ -46,14 +51,13 @@ var snapShot = function(){
             if (err) {
                 console.log('error taking image', err);
             } else {
+
                 servo.move(servo1, position);
                 position += 0.2;
                 if (position > 1) {
                     position = 0; // Reset servo position
                 }
-                console.log(count);
-                //notificationLED.low();
-                // Name the image
+
                 var name = 'picture-' + Date.now() + '.jpg';
                 // Save the image
                 console.log('Picture saving as', name, '...');
